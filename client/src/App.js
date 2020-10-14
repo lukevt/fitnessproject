725e3fdc0531313b1336e105e8ea23191457e48d
lfcsvt <lfcsvt@gmail.com>
2020-10-14 17:23:55 +0100

    new header implemented

--- client/src/App.js
+++ client/src/App.js
@@ -6,37 +6,13 @@ import Trainers from './components/Trainers';
 import Login from './components/Login';
 import Register from './components/Register';
 import Profile from './components/Profile';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/login">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="/register" tabindex="-1" aria-disabled="true">Sign Up</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="/profile" tabindex="-1" aria-disabled="true">Profile</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link " href="/trainers" tabindex="-1" aria-disabled="true">Trainers</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <Navbar/>
         <Switch>
           <Route exact path="/" component={Landing}/>
           <Route path="/trainers" component={Trainers}/>
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Navbar.css'
+import {Link, NavLink} from 'react-router-dom'
+const Navbar = () => (
+    <nav className="row bg-dark main-nav">
+        <div className="col-2">
+            <Link to="/" className="nav-logo-text">
+                <h4>FitBuddy</h4>
+            </Link>    
+        </div>
+        <div className="col-6 offset-2">
+            <ul className="main-menu">
+                <li>
+                    <NavLink to="/" activeClassName="active" exact>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/login" activeClassName="active">Login</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/register" activeClassName="active">Sign Up</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/trainers" activeClassName="active">Trainers</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/profile" activeClassName="active">Profile</NavLink>
+                </li>
+            </ul>
+        </div>
+        
+    </nav>
+);
+
+export default Navbar;
